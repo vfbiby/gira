b02c19f427ac89feace77cf10239613167ece368
James Zhang <liulfbaby@gmail.com>
2021-02-16 02:07:26 +0800

    feat: tdd out a welcome page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Unauthenticated", () => {
+  it("should show login page default", () => {
+    render(<App />);
+    expect(screen.getByText("Username")).toBeInTheDocument();
+    expect(screen.getByText("Password")).toBeInTheDocument();
+  });
+});
+
+describe("Authenticated", () => {
+  it("should render authorized page", () => {
+    render(
+      <App
+        user={{
+          id: 1,
+          name: "bb",
+          email: "7599@qq.com",
+          token: "valid-token",
+        }}
+      />
+    );
+    expect(screen.getByText("Welcome bb")).toBeInTheDocument();
+  });
 });
