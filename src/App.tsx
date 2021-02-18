79f14ac870c4da90367c20d43782747e840bde20
James Zhang <liulfbaby@gmail.com>
2021-02-18 12:02:42 +0800

    feat: a simple way of dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
+import React from "react";
 import "./App.css";
 import { useAuth } from "./utils/hooks";
 
 function App() {
   const user = useAuth();
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  React.useEffect(() => {
+    darkMode
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
+  }, [darkMode]);
   return (
-    <div className="App">
-        {user ? (
-          <div>Welcome {user.name}</div>
-        ) : (
-          <ul className="text-gray-500">
-            <li>Username</li>
-            <li>Password</li>
-          </ul>
-        )}
+    <div className="dark:bg-gray-700 App">
+      {user ? (
+        <div>Welcome {user.name}</div>
+      ) : (
+        <ul className="text-gray-500 dark:text-white">
+          <li>Username</li>
+          <li>Password</li>
+          <button
+            type="button"
+            className="px-2 py-1 rounded-lg"
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            Dark mode
+          </button>
+        </ul>
+      )}
     </div>
   );
 }
