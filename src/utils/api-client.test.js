5840d62d894766792dfe1c509a62a109eb9b718c
James Zhang <liulfbaby@gmail.com>
2021-02-19 21:43:04 +0800

    feat: api-client add post method support

--- src/utils/api-client.test.js
+++ src/utils/api-client.test.js
@@ -2,14 +2,24 @@ import { getToken, localStorageKey } from "../auth-provider";
 import { client } from "./api-client";
 
 describe("Api-client", () => {
+  it("should support post method", async () => {
+    const result = await client("/login", { method: "POST" }).then();
+    expect(result.message).toBe("hello world!");
+  });
+
+  it("should support get method", async () => {
+    const result = client("/me").then();
+    await expect(result).rejects.toEqual("Please re-authenticate.");
+  });
+
   it("should delete token, refresh, throw error when get 401 status code", async () => {
     expect(getToken()).toBe(undefined);
     window.localStorage.setItem(localStorageKey, "valid-token");
     expect(getToken()).toBe("valid-token");
 
-    const result = client("http://localhost/me", { token: undefined }).then();
+    const result = client("/me", { token: undefined }).then();
 
-    await expect(result).rejects.toEqual('Please re-authenticate.');
+    await expect(result).rejects.toEqual("Please re-authenticate.");
     expect(getToken()).toBe(undefined);
   });
 });
--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -4,19 +4,23 @@ interface Config extends RequestInit {
   token?: string;
 }
 
-export const client = async (endpoint: string, { token }: Config = {}) => {
+export const client = async (
+  endpoint: string,
+  { token, ...restConfig }: Config = {}
+) => {
   const defaultConfig = {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: token ? `Bearer ${token}` : "",
     },
+    ...restConfig,
   };
 
   return window.fetch(endpoint, defaultConfig).then(async (response) => {
     if (response.status === 401) {
       Auth.logout();
-      return Promise.reject('Please re-authenticate.')
+      return Promise.reject("Please re-authenticate.");
     }
     return await response.json();
   });
