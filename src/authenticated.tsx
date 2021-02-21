7ef888ca567d1e85c9071ff0d933a1ef17bf52b0
James Zhang <liulfbaby@gmail.com>
2021-02-21 16:48:43 +0800

    feat: header nav has shadow

--- src/authenticated.tsx
+++ src/authenticated.tsx
@@ -6,7 +6,7 @@ const AuthenticatedApp = () => {
 
   return (
     <>
-      <header className="bg-gray-200 dark:bg-gray-800">
+      <header className="shadow-lg dark:bg-gray-800">
         <div className="flex items-center justify-between p-2 px-3">
           <div className="dark:text-white">
             <a href="#"></a>
