d3007a0bd2f70120f8fbccf6aa7175422e1f76d5
James Zhang <liulfbaby@gmail.com>
2021-02-24 01:53:59 +0800

    chore: make the param of useProjects optional

--- src/utils/use-projects.ts
+++ src/utils/use-projects.ts
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { ProjectProps } from "screens/project-list";
 import { client } from "./api-client";
+import { cleanObject } from "./clean-object";
 import { useAsync } from "./use-async";
 
-export const useProjects = (param: Partial<ProjectProps>) => {
+export const useProjects = (param?: Partial<ProjectProps>) => {
   const { run, ...rest } = useAsync<ProjectProps[] | null>();
 
   useEffect(() => {
-    run(client("/projects", { data: param }));
+    run(client("/projects", { data: cleanObject(param || {}) }));
   }, [param]);
 
   return rest;
