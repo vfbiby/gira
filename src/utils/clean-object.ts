70a2fc2bbad01c8559334d88c5a42080a969147a
James Zhang <liulfbaby@gmail.com>
2021-02-23 14:40:26 +0800

    chore: refactoring cleanObject

--- src/utils/clean-object.ts
+++ src/utils/clean-object.ts
@@ -1,10 +1,10 @@
-const isVoid = (value: unknown) =>
+export const isVoid = (value: unknown) =>
   value === "" || value === null || value === undefined;
 
 export const cleanObject = (obj: { [key: string]: unknown }) => {
   let result = { ...obj };
-  Object.keys(result).forEach((key) => {
-    if (isVoid(result[key])) delete result[key];
-  });
+  Object.keys(result).forEach(
+    (key) => isVoid(result[key]) && delete result[key]
+  );
   return result;
 };
