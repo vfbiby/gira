65e96925393a1bac71dc6ea0bf7a34c344596eb8
James Zhang <liulfbaby@gmail.com>
2021-02-26 01:35:32 +0800

    feat: add kanban and epic screen

---
+++ src/screens/kanban/index.tsx
@@ -0,0 +1,3 @@
+export const KanbanScreen = () => {
+  return <h1 className="dark:text-white">KanbanScreen</h1>;
+};
--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,6 +1,35 @@
-import { useParams } from "react-router-dom";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { EpicScreen } from "screens/epic";
+import { KanbanScreen } from "screens/kanban";
 
 export const ProjectScreen = () => {
-  const { projectId } = useParams();
-  return <h1 className="dark:text-white">ProjectScreen {projectId}</h1>;
+  return (
+    <>
+      <h1 className="dark:text-white">ProjectScreen</h1>
+      <div className="flex dark:text-white">
+        <div className="w-48 dark:bg-gray-700">
+          <ul>
+            <li className="px-4 py-2 hover:bg-gray-500">
+              <Link to="kanban">
+                <span className="block w-full h-full">看板</span>
+              </Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-500">
+              <Link to="epic">
+                <span className="block w-full h-full">任务组</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="dark:bg-gray-500 h-full w-full">
+          <span>thiskd</span>
+          <Routes>
+            <Route path="/kanban" element={<KanbanScreen />} />
+            <Route path="/epic" element={<EpicScreen />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 };
