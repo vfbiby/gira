2f6f38bc5000ee50423910e1e566bfc16b7077e7
James Zhang <liulfbaby@gmail.com>
2021-02-26 00:27:31 +0800

    chore: delete console.log

--- src/utils/use-documentTitle.ts
+++ src/utils/use-documentTitle.ts
@@ -5,11 +5,10 @@ export const useDocumentTitle = (newTitle: string) => {
 
   useEffect(() => {
     oldTitle.current = document.title;
-    console.log("title when loading", oldTitle.current);
     document.title = newTitle;
+
     return () => {
-      console.log("title when unLoading", oldTitle.current);
       document.title = oldTitle.current;
     };
-  }, [oldTitle]);
+  }, []);
 };
