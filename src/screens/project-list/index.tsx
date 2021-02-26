85e004b20e9664455ccb7ecd033ece08aff16410
James Zhang <liulfbaby@gmail.com>
2021-02-26 23:08:22 +0800

    feat: add whyDidYouRender and avoid infinit refresh

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -43,3 +43,4 @@ export const ProjectsListScreen = () => {
     </div>
   );
 };
+ProjectsListScreen.whyDidYouRender = true;
--- src/utils/useUrlQueryParam.ts
+++ src/utils/useUrlQueryParam.ts
@@ -1,12 +1,17 @@
+import { useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
   const [searchParams, setSearchParam] = useSearchParams();
 
   return [
-    keys.reduce((prev, key) => {
-      return { ...prev, [key]: searchParams.get(key) || "" };
-    }, {} as { [key in K]: string }),
+    useMemo(
+      () =>
+        keys.reduce((prev, key) => {
+          return { ...prev, [key]: searchParams.get(key) || "" };
+        }, {} as { [key in K]: string }),
+      [searchParams]
+    ),
     setSearchParam,
   ] as const;
 };
