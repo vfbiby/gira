707940cc31785172380f09551114f42c11947337
James Zhang <liulfbaby@gmail.com>
2021-02-28 02:13:15 +0800

    feat: project list add pin icon

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,3 +1,4 @@
+import { IconPin } from "components/icon/icon-pin";
 import { PageLoading } from "components/page-loading";
 import dayjs from "dayjs";
 import React from "react";
@@ -20,16 +21,21 @@ export const ProjectsList = ({
       <table className="w-full table-fixed">
         <thead>
           <tr className="px-4 py-2 text-lg font-bold text-center">
+            <td className="flex items-center justify-center px-4 py-2 w-1/10">
+              <IconPin className="block w-8 h-8 text-gray-400 fill-current dark:text-gray-600" />
+            </td>
             <td className="w-1/5 px-4 py-2">名称</td>
             <td className="w-1/5 px-4 py-2">部门</td>
             <td className="w-1/5 px-4 py-2">负责人</td>
             <td className="w-1/5 px-4 py-2">创建时间</td>
+            <td className="px-4 py-2 w-1/10"></td>
           </tr>
         </thead>
         <tbody>
           {projects?.map((project) => {
             return (
               <tr key={project.id} className="">
+                <td className="px-4 py-2 border border-blue-500"></td>
                 <td className="px-4 py-2 border border-blue-500">
                   <Link to={`${project.id}`}>{project.name}</Link>
                 </td>
