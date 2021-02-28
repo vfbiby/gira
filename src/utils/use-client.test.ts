c05e98a69ef5dedecec6a37bf81acfcf518fdc7d
James Zhang <liulfbaby@gmail.com>
2021-02-28 19:10:26 +0800

    refactor: useClient extract some function

--- src/utils/use-client.test.ts
+++ src/utils/use-client.test.ts
@@ -9,14 +9,19 @@ jest.spyOn(Http, "client");
 
 describe("useClient", () => {
   beforeEach(() => {
+    updateAuthContext();
+  });
+
+  const updateAuthContext = (user?: { [key: string]: string | number }) => {
     (mocked(useAuth) as jest.Mock).mockReturnValue({
       user: {
         id: 1,
         name: "vf",
         token: "valid-user-token",
+        ...user,
       },
     });
-  });
+  };
 
   it("should take token when send a request", async () => {
     const { result } = renderHook(() => useClient());
@@ -31,12 +36,63 @@ describe("useClient", () => {
 
   it("should take data when send a request", function () {
     const { result } = renderHook(() => useClient());
-    expect(typeof result.current).toStrictEqual("function");
 
     result.current("/me", { data: { name: "bb", age: 35 } });
     expect(Http.client).toHaveBeenCalledWith("/me", {
       token: "valid-user-token",
       data: { name: "bb", age: 35 },
+    });
+  });
+
+  it("should pass extra params like method, headers to client", function () {
+    const { result } = renderHook(() => useClient());
+    result.current("/me", {
+      method: "POST",
+      headers: { Authorization: "bear 234" },
+    });
+    expect(Http.client).toHaveBeenCalledWith("/me", {
+      token: "valid-user-token",
+      method: "POST",
+      headers: { Authorization: "bear 234" },
+    });
+  });
+
+  it("should return not same instance if token change", function () {
+    const { result, rerender } = renderHook(() => useClient());
+    const oldCallback = result.current;
+
+    updateAuthContext({
+      token: "second-token",
+    });
+
+    rerender();
+    expect(oldCallback).not.toStrictEqual(result.current);
+  });
+
+  it("should return same instance if token not change", function () {
+    const { result, rerender } = renderHook(() => useClient());
+    const oldCallback = result.current;
+    rerender();
+    expect(oldCallback).toStrictEqual(result.current);
+  });
+
+  it("should use updated token", function () {
+    const { result, rerender } = renderHook(() => useClient());
+
+    result.current("/me", { data: { name: "bb", age: 35 } });
+    expect(Http.client).toHaveBeenCalledWith("/me", {
+      token: "valid-user-token",
+      data: { name: "bb", age: 35 },
+    });
+
+    updateAuthContext({
+      token: "second-token",
+    });
+    rerender();
+    result.current("/me");
+
+    expect(Http.client).toHaveBeenCalledWith("/me", {
+      token: "second-token",
     });
   });
 });
--- src/utils/use-client.ts
+++ src/utils/use-client.ts
@@ -1,10 +1,14 @@
+import { useCallback } from "react";
 import { client } from "./api-client";
 import { useAuth } from "./hooks";
 
 export const useClient = () => {
   const { user } = useAuth();
 
-  return (...[url, config]: Parameters<typeof client>) => {
-    client(url, { ...config, token: user?.token });
-  };
+  return useCallback(
+    (...[url, config]: Parameters<typeof client>) => {
+      client(url, { ...config, token: user?.token });
+    },
+    [user?.token]
+  );
 };
