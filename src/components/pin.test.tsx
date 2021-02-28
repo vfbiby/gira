fc5d045920fac805162b814832d072c7cdf7ee33
James Zhang <liulfbaby@gmail.com>
2021-02-28 22:19:58 +0800

    fix: pin test pin_checked_color

--- src/components/pin.test.tsx
+++ src/components/pin.test.tsx
@@ -2,6 +2,8 @@ import { fireEvent, render } from "@testing-library/react";
 import React from "react";
 import { Pin } from "./pin";
 
+const PIN_CHECKED_COLOR = "text-yellow-300";
+
 describe("Pin", () => {
   it("should be empty content when rendering", () => {
     const { container } = render(<Pin checked={false} />);
@@ -11,14 +13,14 @@ describe("Pin", () => {
   it("should not show checked color when it is not checked", () => {
     const { container } = render(<Pin checked={false} />);
     expect(container.querySelector("svg")?.classList).not.toContain(
-      "text-yellow-200"
+      PIN_CHECKED_COLOR
     );
   });
 
   it("should show checked color when it is checked", () => {
     const { container } = render(<Pin checked={true} />);
     expect(container.querySelector("svg")?.classList).toContain(
-      "text-yellow-200"
+      PIN_CHECKED_COLOR
     );
   });
 
@@ -26,35 +28,37 @@ describe("Pin", () => {
     const { container } = render(<Pin checked={false} />);
 
     expect(container.querySelector("svg")?.classList).not.toContain(
-      "text-yellow-200"
+      PIN_CHECKED_COLOR
     );
     fireEvent.click(container.querySelector("div") as Element);
 
     expect(container.querySelector("svg")?.classList).toContain(
-      "text-yellow-200"
+      PIN_CHECKED_COLOR
     );
     fireEvent.click(container.querySelector("div") as Element);
 
     expect(container.querySelector("svg")?.classList).not.toContain(
-      "text-yellow-200"
+      PIN_CHECKED_COLOR
     );
   });
 
   it("should call callback when click", () => {
-    const onChange = jest.fn().mockImplementation(()=>{});
+    const onChange = jest.fn().mockImplementation(() => {});
     const { container } = render(<Pin checked={false} onChange={onChange} />);
 
     fireEvent.click(container.querySelector("svg") as Element);
     expect(onChange).toHaveBeenCalledTimes(1);
     expect(onChange).toHaveBeenCalledWith(false);
-  })
+  });
 
-  it("should be inactive when set to disabled", function(){
-    const onChange = jest.fn().mockImplementation(()=>{});
-    const { container } = render(<Pin checked={false} disabled={true} onChange={onChange} />);
+  it("should be inactive when set to disabled", () => {
+    const onChange = jest.fn().mockImplementation(() => {});
+    const { container } = render(
+      <Pin checked={false} disabled={true} onChange={onChange} />
+    );
 
     expect(container.querySelector("svg")?.classList).not.toContain(
-      "text-yellow-200"
+      PIN_CHECKED_COLOR
     );
 
     fireEvent.click(container.querySelector("svg") as Element);
@@ -62,8 +66,7 @@ describe("Pin", () => {
     expect(onChange).toHaveBeenCalledTimes(0);
     expect(onChange).not.toHaveBeenCalled();
     expect(container.querySelector("svg")?.classList).not.toContain(
-      "text-yellow-200"
+      PIN_CHECKED_COLOR
     );
-  })
-
+  });
 });
