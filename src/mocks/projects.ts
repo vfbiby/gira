81411285ae0d13c76fd63ccf35874face3ce2a3e
James Zhang <liulfbaby@gmail.com>
2021-03-01 20:52:59 +0800

    fix: add ProjectProps

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -1,18 +1,23 @@
 import { rest } from "msw";
+import { ProjectProps } from "screens/project-list";
 import { ls } from "utils/ls-client";
 
 export const projectsHandlers = [
   rest.get("/projects", (req, res, ctx) => {
     const personId = Number(req.url.searchParams.get("personId"));
     const name = req.url.searchParams.get("name");
-    const projects = ls.get('projects');
+    const projects = ls.get("projects");
     return res(
       ctx.delay(),
       ctx.json(
         personId
-          ? projects.filter((project) => project.personId === personId)
+          ? projects.filter(
+              (project: ProjectProps) => project.personId === personId
+            )
           : name
-          ? projects.filter((project) => project.name.includes(name))
+          ? projects.filter((project: ProjectProps) =>
+              project.name.includes(name)
+            )
           : projects
       )
     );
