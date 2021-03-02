3dae560d6f49dd5db84e1d5fbc4a85bbded6f58a
James Zhang <liulfbaby@gmail.com>
2021-03-03 02:12:16 +0800

    feat: add react-query

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -2,14 +2,19 @@ import React, { ReactNode } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./auth-context";
 import { DarkProvider } from "./dark-context";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const AppProviders = ({ children }: { children: ReactNode }) => {
+  const queryClient = new QueryClient();
+
   return (
-    <DarkProvider>
-      <Router>
-        <AuthProvider>{children}</AuthProvider>
-      </Router>
-    </DarkProvider>
+    <QueryClientProvider client={queryClient}>
+      <DarkProvider>
+        <Router>
+          <AuthProvider>{children}</AuthProvider>
+        </Router>
+      </DarkProvider>
+    </QueryClientProvider>
   );
 };
 
