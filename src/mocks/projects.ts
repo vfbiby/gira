de2ddc57b8a2c05c77c932f5c977d6f734051a9c
James Zhang <liulfbaby@gmail.com>
2021-03-06 00:47:59 +0800

    feat: add post projects mock api

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -23,6 +23,33 @@ export const projectsHandlers = [
     );
   }),
 
+  rest.post<ProjectProps>("/projects", (req, res, ctx) => {
+    const projects = ls.get("projects");
+    const projectId = projects[projects.length - 1].id + 1;
+    const { name, organization, personId } = req.body;
+    if (!name || !organization || !personId) {
+      return res(
+        ctx.status(404),
+        ctx.json({
+          status: 404,
+          message: "please provide name organization and personId",
+        })
+      );
+    }
+    const project = {
+      id: projectId,
+      name,
+      organization,
+      personId,
+      pin: false,
+      created: new Date(),
+    };
+    projects.push(project);
+    ls.set("projects", projects);
+
+    return res(ctx.json(project));
+  }),
+
   rest.get("/users", (_, res, ctx) => {
     return res(ctx.json(ls.get("users")));
   }),
